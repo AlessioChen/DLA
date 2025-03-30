47996312232cc0424462d703d7dcddb682faebf6
Alessio Chen <alessiochen98@gmail.com>
2025-03-30 21:06:32 +0200

    update MLP to have multiple hidden layers

--- lab1/MLP.py
+++ lab1/MLP.py
@@ -4,14 +4,31 @@ import torch.nn.functional as F
 
 class MLP(nn.Module):
 
-    def __init__(self, input_size: int, hidden_size: int, num_classes: int) -> None:
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, num_layers: int = 2) -> None:
         super().__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, num_classes)
-    
-    def forward(self, x: torch.Tensor):
-        x = x.view(x.size(0), -1) # Flatten the input 
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        self.input_layer = nn.Linear(input_size, hidden_size)
+        self.hidden_layers = nn.ModuleList()
 
+        for _ in range(num_layers - 2):
+            self.hidden_layers.append(nn.Linear(hidden_size, hidden_size))
+
+
+        self.output_layer = nn.Linear(hidden_size, output_size)
+        self.activation = nn.ReLU()
+
+
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        if len(x.shape) > 2: 
+            x = x.view(x.size(0), -1) # Flatten the input 
+        
+        x = self.input_layer(x)
+        x = self.activation(x)
+
+        for layer in self.hidden_layers: 
+            x = layer(x)
+            x = self.activation(x)
+        
+        x = self.output_layer(x)
         return x
